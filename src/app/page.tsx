a81cf139673883d2956845873cf16f332bf88aaf
isaacrossi <isaacjohnrossi@gmail.com>
2026-02-14 19:31:33 +1100

    Configured eslint and created a tickets page

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h2 className="text-6xl">Home Page</h2>;
+};
+
+export default HomePage;
---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,5 @@
+const TicketsPage = () => {
+  return <h2 className="text-6xl">Tickets Page</h2>;
+};
+
+export default TicketsPage;
