8d1a32ccf4b6d2b7f0732872921e7985141bbefa
isaacrossi <isaacjohnrossi@gmail.com>
2026-02-17 20:51:41 +1100

    Define path constants in path.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { ticketsPath } from "@/src/paths";
 
 const HomePage = () => {
   return (
     <div>
       <h2 className="text-lg">Home Page</h2>
-      <Link href="/tickets">Go to tickets</Link>
+      <Link href={ticketsPath()}>Go to tickets</Link>
     </div>
   );
 };
