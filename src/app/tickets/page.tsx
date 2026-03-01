df2b4fab56887d19cd9bd3854dd401d439da1ce8
isaacrossi <isaacjohnrossi@gmail.com>
2026-03-01 21:22:47 +1100

    refactor: fetch tickets from server instead of client

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,9 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { Heading } from "@/src/components/heading";
 import { TicketItem } from "@/src/features/ticket/components/ticket-item";
 import { getTickets } from "@/src/features/ticket/queries/get-tickets";
-import { Ticket } from "@/src/features/ticket/types";
 
-const TicketsPage = () => {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-
-  //without react-query, we would have to do something like this to fetch the tickets on the client side
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const result = await getTickets();
-      setTickets(result);
-    };
-    fetchTickets();
-  }, []); //the empty dependency array means this effect will only run once when the component mounts
+const TicketsPage = async () => {
+  const tickets = await getTickets();
 
   return (
     <div className="flex-1 flex flex-col gap-y-8">
