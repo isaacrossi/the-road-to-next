e4de30a2c632e9d7643bfa5f8d2c762eacac87b7
isaacrossi <isaacjohnrossi@gmail.com>
2026-03-02 17:56:35 +1100

    refactor: extract TicketList and wrap with Suspense for better UX

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,18 +1,15 @@
+import { Suspense } from "react";
 import { Heading } from "@/src/components/heading";
-import { TicketItem } from "@/src/features/ticket/components/ticket-item";
-import { getTickets } from "@/src/features/ticket/queries/get-tickets";
+import { Spinner } from "@/src/components/spinner";
+import { TicketList } from "@/src/features/ticket/components/ticket-list";
 
-const TicketsPage = async () => {
-  const tickets = await getTickets();
-
+const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
