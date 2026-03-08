934a2e0b25b53be7ed3b69de934ef560fbe63f69
isaacrossi <isaacjohnrossi@gmail.com>
2026-03-08 15:20:07 +1100

    feat: implement prisma singleton and refactor data queries

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,10 +1,9 @@
-import { initialTickets } from "@/src/data";
-import { Ticket } from "../types";
+import { prisma } from "@/lib/prisma";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
-
-  return maybeTicket || null;
+export const getTicket = async (id: string) => {
+  return await prisma.ticket.findUnique({
+    where: {
+      id,
+    },
+  });
 };
