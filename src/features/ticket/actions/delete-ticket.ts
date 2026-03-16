01a7d81d6730905a3e1acdc7f31891c2f62ab3bb
isaacrossi <isaacjohnrossi@gmail.com>
2026-03-16 19:42:51 +1100

    feat: redirect user when a ticket is deleted

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { prisma } from "@/src/lib/prisma";
+import { ticketsPath } from "@/src/paths";
 
 export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({ where: { id } });
+
+  redirect(ticketsPath());
 };
