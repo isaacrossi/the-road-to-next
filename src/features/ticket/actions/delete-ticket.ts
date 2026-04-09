9d722417893c01f7df639b34db483f3249e5f0f7
isaacrossi <isaacjohnrossi@gmail.com>
2026-04-09 19:51:57 +1000

    refactor: revalidate tickets path when deleting a ticket

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { prisma } from "@/src/lib/prisma";
 import { ticketsPath } from "@/src/paths";
@@ -7,5 +8,6 @@ import { ticketsPath } from "@/src/paths";
 export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({ where: { id } });
 
+  revalidatePath(ticketsPath());
   redirect(ticketsPath());
 };
