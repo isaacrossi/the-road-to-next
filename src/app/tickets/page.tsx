8a70903ccd020c90fb21b6a9a3eab1c854b855a9
isaacrossi <isaacjohnrossi@gmail.com>
2026-04-12 19:47:53 +1000

    feat: add initial UI shell for ticket creation card

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,12 +3,28 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Heading } from "@/src/components/heading";
 import { Placeholder } from "@/src/components/placeholder";
 import { Spinner } from "@/src/components/spinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/src/components/ui/card";
 import { TicketList } from "@/src/features/ticket/components/ticket-list";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle></CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>TicketCreateForm</CardContent>
+      </Card>
+
       <ErrorBoundary fallback={<Placeholder label="Something went wrong!" />}>
         <Suspense fallback={<Spinner />}>
           <TicketList />
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/src/lib/prisma";
 
 export const getTicket = async (id: string) => {
+  console.log("getTicket", id);
+
   return await prisma.ticket.findUnique({
     where: {
       id,
