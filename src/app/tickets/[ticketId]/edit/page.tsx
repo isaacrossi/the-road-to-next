71a74429ee51e9654d9aedfa7462a4bfc69a42a7
isaacrossi <isaacjohnrossi@gmail.com>
2026-04-17 15:47:24 +1000

    feat(tickets): add edit page for individual tickets

---
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -0,0 +1,31 @@
+import { CardCompact } from "@/src/components/card-compact";
+import { getTicket } from "@/src/features/ticket/queries/get-ticket";
+import notFound from "../not-found";
+
+type TicketEditPageProps = {
+  params: {
+    ticketId: string;
+  };
+};
+
+const TicketEditPage = async ({ params }: TicketEditPageProps) => {
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId);
+
+  if (!ticket) {
+    notFound();
+  }
+
+  return (
+    <div className="flex-1 flex flex-col justify-center items-center">
+      <CardCompact
+        title="Edit Ticket"
+        description="Edit an existing ticket"
+        className="w-full max-w-[420px] animate-fade-in-from-top"
+        content={<></>}
+      />
+    </div>
+  );
+};
+
+export default TicketEditPage;
