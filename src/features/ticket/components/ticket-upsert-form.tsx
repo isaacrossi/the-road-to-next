9b8e57c2d4e4dfc452ae95febbb627afdfc9d533
isaacrossi <isaacjohnrossi@gmail.com>
2026-04-20 19:40:30 +1000

    feat: add loading state and spinner to ticket form submission

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { Ticket } from "@prisma/client";
+import { LucideLoaderCircle } from "lucide-react";
+import { useTransition } from "react";
 import { Button } from "@/src/components/ui/button";
 import { Input } from "@/src/components/ui/input";
 import { Label } from "@/src/components/ui/label";
@@ -10,18 +14,29 @@ type TicketUpdateFormProps = {
 };
 
 const TicketUpsertForm = ({ ticket }: TicketUpdateFormProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const upsertTicketAction = (formData: FormData) => {
+    startTransition(async () => {
+      // Just call it normally! No binding required.
+      await upsertTicket(ticket?.id, formData);
+    });
+  };
+
   return (
-    <form
-      action={upsertTicket.bind(null, ticket?.id)}
-      className="flex flex-col gap-y-2"
-    >
+    <form action={upsertTicketAction} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
       <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
 
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+      <Button disabled={isPending} type="submit">
+        {isPending && (
+          <LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+        )}
+        {ticket ? "Edit" : "Create"}
+      </Button>
     </form>
   );
 };
