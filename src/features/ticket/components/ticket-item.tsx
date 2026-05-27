28a4c1cdc3a39ea4977fc0a10c5b0590c57702c0
isaacrossi <isaacjohnrossi@gmail.com>
2026-05-27 16:15:59 +1000

    feat: implement ConfirmDialog component when deleting a ticket

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -7,6 +7,7 @@ import {
   LucideTrash,
 } from "lucide-react";
 import Link from "next/link";
+import { ConfirmDialog } from "@/components/confirm-dialog";
 import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Card,
@@ -47,12 +48,17 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
     </Link>
   );
 
+  // action will run when we confirm the dialog
+  // We pass a trigger prop so we can decide what the button looks like
   const deleteButton = (
-    <form action={deleteTicket.bind(null, ticket.id)}>
-      <Button variant="outline" size="icon">
-        <LucideTrash className="h-4 w-4" />
-      </Button>
-    </form>
+    <ConfirmDialog
+      action={deleteTicket.bind(null, ticket.id)}
+      trigger={
+        <Button variant="outline" size="icon">
+          <LucideTrash className="h-4 w-4" />
+        </Button>
+      }
+    />
   );
 
   const moreMenu = (
